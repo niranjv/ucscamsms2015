93d66b49e5019e16516e87f95eb88a6f5b77b29b
Niranjan <niranjanv+github@gmail.com>
2015-03-17 22:52:42 -0700

    #9 - adjust knitr options to generate PDF file correctly; copy plots to the correct location before running xelatex; run xelatex twice since ToC is not generated after the 1st pass; add comments describing what is going on in this script

--- report/build-report.r
+++ report/build-report.r
@@ -16,13 +16,41 @@ render_chapter <- function(src) {
   if (!needs_update(src, dest)) return()
 
   message("Rendering ", src)
-  command <- bquote(rmarkdown::render(.(src), bookdown::tex_chapter(),
-    output_dir = "report/tex", quiet = TRUE, env = globalenv()))
-  writeLines(deparse(command), "run.r")
+
+  # This is the script that will be executed for each *.rmd file
+  # in the the source_clean() function below
+  r_filename <- 'run.r'
+
+  # Most of the R commands below set the options for knitr
+  # that are different from those defined in bookdown::tex_chapter()
+  #FIXME modify bookdown::tex_chapter() so these options can be passed
+  # directly to this function instead of doing it this way
+  command <- bquote(o <- bookdown::tex_chapter())
+  cat(paste(deparse(command), '\n'), file=r_filename, append=FALSE)
+
+  command <- bquote(o$pandoc$from <-
+    "markdown+autolink_bare_uris-auto_identifiers+tex_math_single_backslash")
+  cat(paste(deparse(command), '\n'), file=r_filename, append=TRUE)
+
+  command <- bquote(o$knitr$opts_chunk$fig.caption <- 'yes')
+  cat(paste(deparse(command), '\n'), file=r_filename, append=TRUE)
+
+  command <- bquote(o$knitr$opts_chunk$fig.width <- 6)
+  cat(paste(deparse(command), '\n'), file=r_filename, append=TRUE)
+
+  command <- bquote(o$knitr$opts_chunk$fig.height <- 6)
+  cat(paste(deparse(command), '\n'), file=r_filename, append=TRUE)
+
+  command <- bquote(rmarkdown::render(.(src), o, output_dir = "report/tex",
+    quiet = TRUE, env = globalenv()))
+  cat(paste(deparse(command), '\n'), file=r_filename, append=TRUE)
+
   on.exit(unlink("run.r"))
   source_clean("run.r")
 }
 
+# Run 'run.r' using a new instance of R since we are using lapply
+# to process each *.rmd file
 source_clean <- function(path) {
   r_path <- file.path(R.home("bin"), "R")
   cmd <- paste0(shQuote(r_path), " --quiet --file=", shQuote(path))
@@ -35,17 +63,28 @@ source_clean <- function(path) {
   }
 }
 
+
+# knitr converts *.rmd -> *.md
+# pandoc converts *.md -> *.tex
 chapters <- dir(".", pattern = "\\.rmd$")
 lapply(chapters, render_chapter)
 
-# Copy across additional files -------------------------------------------------
-file.copy("report/report.tex", "report/tex/", recursive = TRUE)
-# file.copy("diagrams/", "book/tex/", recursive = TRUE)
 
-# Build tex file ---------------------------------------------------------------
-# (build with Rstudio to find/diagnose errors)
+# Copy report.tex and all figures to report/tex/ folder since xelatex expects
+# to find them there
+# report.tex is the parent tex file that includes the tex files generated above
+file.copy("report/report.tex", "report/tex/", recursive = TRUE)
+file.copy("figures/", "report/tex/", recursive = TRUE)
+
+
+# xelatex converts report.tex -> report.pdf
+# Run xelatex twice to generate ToC properly since ToC is missing from the
+# PDF file after the 1st pass
 old <- setwd("report/tex")
+system("xelatex -interaction=batchmode report ")
 system("xelatex -interaction=batchmode report ")
 setwd(old)
 
+
+# Copy report.pdf outside the tex folder
 file.copy("report/tex/report.pdf", "report/report.pdf", overwrite = TRUE)
